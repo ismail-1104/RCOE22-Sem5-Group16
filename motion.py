1db6ec2691c3ac34bfc7eae9e4025782899eecf2
Abdulnabi Shaikh <abdulnabishaikh6545@eng.rizvi.edu.in>
2022-10-22 16:05:35 +0530

    added two frames, calculating their difference for motion detection

--- motion.py
+++ motion.py
@@ -1,7 +1,10 @@
 import cv2
+from numpy import diff
 cam = cv2.VideoCapture(0)
 while cam.isOpened():
-    ret, frame = cam.read()
+    ret, frame1 = cam.read()
+    ret, frame2 = cam.read()
+    diff = cv2.absdiff(frame1, frame2)
     if cv2.waitKey(10) == ord('e'):
         break
-    cv2.imshow('Security Camera', frame)
+    cv2.imshow('Security Camera', diff)
