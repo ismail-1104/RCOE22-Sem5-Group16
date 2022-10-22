ee7df2b91f495184159fa25fe0ecad5a58d5cea7
Abdulnabi Shaikh <abdulnabishaikh6545@eng.rizvi.edu.in>
2022-10-22 16:13:13 +0530

    added threshold value to detect big motions

--- motion.py
+++ motion.py
@@ -1,3 +1,4 @@
+from turtle import color
 import cv2
 from numpy import diff
 cam = cv2.VideoCapture(0)
@@ -6,6 +7,8 @@ while cam.isOpened():
     ret, frame2 = cam.read()
     diff = cv2.absdiff(frame1, frame2)
     gray = cv2.cvtColor(diff, cv2.COLOR_RGB2BGR)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    color, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
     if cv2.waitKey(10) == ord('e'):
         break
-    cv2.imshow('Security Camera', gray)
+    cv2.imshow('Security Camera', blur)
