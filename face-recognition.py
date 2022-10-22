579eb6e439443e294b613158dafe591abf244e79
Mohammed Ismail <shaikhismail1104@gmail.com>
2022-10-22 17:23:32 +0530

    Code for printing name of the person

--- face-recognition.py
+++ face-recognition.py
@@ -18,3 +18,9 @@ myList = os.listdir(path)
 
 print(myList)
 
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    image.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+
+print(classNames)
