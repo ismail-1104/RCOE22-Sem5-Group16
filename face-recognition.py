abda41db72da64b1f279a0ed966dee6c0a935b13
Mohammed Ismail <shaikhismail1104@gmail.com>
2022-10-22 17:15:52 +0530

    Importing all libraries

---
+++ face-recognition.py
@@ -0,0 +1,11 @@
+from importlib.resources import path
+import cv2
+import numpy as np
+import face_recognition
+import os
+import winsound
+
+# imgIsmail = face_recognition.load_image_file('images/IMG_20220710_091836.jpg')
+# imgIsmail = cv2.cvtColor(imgIsmail,cv2.COLOR_BGR2RGB)
+
+cam = cv2.VideoCapture(0)
