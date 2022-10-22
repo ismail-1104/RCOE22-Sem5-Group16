5e9bf1771ccba132bf1f475c964bf727ccdf0e9e
Mohammed Ismail <shaikhismail1104@gmail.com>
2022-10-22 17:27:46 +0530

    Reads the camera and encodes it.

--- face-recognition.py
+++ face-recognition.py
@@ -35,3 +35,11 @@ def findEncodings(image):
 
 encodeListKnown = findEncodings(image)
 print('Encoding Completed...')
+
+while True:
+    success, img = cam.read()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
